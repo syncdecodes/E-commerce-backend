13540ef3ba87c567d7e2d5194794e9b0c6db3029
syncdecodes <syncdecodes@gmail.com>
2026-02-13 20:32:54 +0530

    Modified mail.config.js

--- config/mail.config.js
+++ config/mail.config.js
@@ -1,11 +1,15 @@
 const nodemailer = require('nodemailer')
 
 const transporter = nodemailer.createTransport({
-    service: "gmail", 
+    // service: "gmail", 
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASSWORD
-    }
+    },
+    connectionTimeout: 10000, // 10 seconds
 })
 
 module.exports = transporter
